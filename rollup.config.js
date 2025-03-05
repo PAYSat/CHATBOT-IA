2c85a828a6575c41cd0e0a307396bb62dfdead17
jporellana31 <juan.orellana.57@est.ucacue.edu.ec>
2025-03-05 14:45:17 -0500

    nuevas implementaciones para integracion con twilio

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'src/app.ts',
+    input: 'src/app.ts', // Punto de entrada
     output: {
-        file: 'dist/app.js',
-        format: 'esm',
+        file: 'dist/app.js', // Archivo de salida
+        format: 'esm', // Formato de módulo (ES Modules)
     },
     onwarn: (warning) => {
-        if (warning.code === 'UNRESOLVED_IMPORT') return
+        if (warning.code === 'UNRESOLVED_IMPORT') return; // Ignorar advertencias de importaciones no resueltas
     },
-    plugins: [typescript()],
-}
+    plugins: [
+        typescript(), // Compila TypeScript
+        resolve(), // Resuelve módulos de Node.js
+        commonjs(), // Convierte módulos CommonJS a ES6
+    ],
+};
